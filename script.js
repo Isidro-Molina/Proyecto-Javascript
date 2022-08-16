061a5d6dccbedbb1d2b5b80d7b16d78262539ec1
Isidro Molina <isidromolina260@gmail.com>
2022-08-16 19:02:37 -0300

    cambios minimos

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let jugador = document.querySelector('.jugador')
 const puntosUsuario = document.getElementById('puntajeUsuario');
 const puntosComputadora = document.getElementById('puntajeComputadora');
 const tablero = document.querySelector('.tablero');
-const eleccion = document.querySelector('.eleccion');const resultado = document.querySelector('.resultado')
+const eleccion = document.querySelector('.eleccion');
+const resultado = document.querySelector('.resultado')
 const piedra = document.getElementById('piedra');
 const papel = document.getElementById('papel');
 const tijera = document.getElementById('tijera');
@@ -36,59 +37,62 @@ function maquina() {
 
 function juego(eleccionUsuario) {
     const eleccionMaquina = maquina();
-    console.log('el usuario eligio ' +eleccionUsuario);
-    console.log('La maquina eligio ' +eleccionMaquina);
+    console.log('el usuario eligio ' + eleccionUsuario);
+    console.log('La maquina eligio ' + eleccionMaquina);
     eleccion.innerHTML = `<h2>Elegiste ${eleccionUsuario}! La maquina eligio ${eleccionMaquina}`
 
     switch (eleccionUsuario) {
         case 'Piedra':
+            eleccionMaquina == 'Tijera' && console.log('Ganaste!!');
+            eleccionMaquina == 'Piedra' && console.log('Empate.');
+            eleccionMaquina == 'Papel' && console.log('Perdiste!!!');
+
             if (eleccionMaquina == 'Tijera') {
-                console.log('Ganaste');
                 resultado.innerHTML = `<h6 class="ganar">Ganaste!</h6>`
             } else if (eleccionMaquina == 'Piedra') {
-                console.log('Empate');
                 resultado.innerHTML = `<h6 class="empatar">Empate!</h6>`
             } else if (eleccionMaquina == 'Papel') {
-                console.log('Perdiste');
                 resultado.innerHTML = `<h6 class="perder">Perdiste!</h6>`
             }
             break
         case 'Papel':
+            eleccionMaquina == 'Piedra' && console.log('Ganaste!!');
+            eleccionMaquina == 'Papel' && console.log('Empate.');
+            eleccionMaquina == 'Tijera' && console.log('Perdiste!!!');
+
             if (eleccionMaquina == 'Piedra') {
-                console.log('Ganaste');
                 resultado.innerHTML = `<h6 class="ganar">Ganaste!</h6>`
             } else if (eleccionMaquina == 'Papel') {
-                console.log('Empate');
                 resultado.innerHTML = `<h6 class="empatar">Empate!</h6>`
             } else if (eleccionMaquina == 'Tijera') {
-                console.log('Perdiste');
                 resultado.innerHTML = `<h6 class="perder">Perdiste!</h6>`
             }
             break
         case 'Tijera':
+            eleccionMaquina == 'Papel' && console.log('Ganaste!!');
+            eleccionMaquina == 'Tijera' && console.log('Empate.');
+            eleccionMaquina == 'Piedra' && console.log('Perdiste!!!');
+
             if (eleccionMaquina == 'Papel') {
-                console.log('Ganaste');
                 resultado.innerHTML = `<h6 class="ganar">Ganaste!</h6>`
             } else if (eleccionMaquina == 'Tijera') {
-                console.log('Empate');
                 resultado.innerHTML = `<h6 class="empatar">Empate!</h6>`
             } else if (eleccionMaquina == 'Piedra') {
-                console.log('Perdiste');
                 resultado.innerHTML = `<h6 class="perder">Perdiste!</h6>`
             }
     }
 }
 
 
-piedra.addEventListener('click', ()=> {
+piedra.addEventListener('click', () => {
     juego('Piedra')
 })
 
-papel.addEventListener('click', ()=> {
+papel.addEventListener('click', () => {
     juego('Papel')
 })
 
-tijera.addEventListener('click', ()=> {
+tijera.addEventListener('click', () => {
     juego('Tijera')
 })
 
@@ -102,14 +106,3 @@ reiniciar.addEventListener('click', () => {
     console.log('Esta jugando ' + JugadorActivo);
     eleccion.innerHTML = `<h3>Elegi una opcion!</h3>`
 })
-
-
-
-
-
-
-
-
-
-
-
